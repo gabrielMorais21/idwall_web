effadcd1cb1504b539cdf14a07ff113e8047e2fa
gabrielMorais21 <gabrielmd908@gmail.com>
2022-03-21 20:18:22 -0300

    tela completa

--- pages/search_user/search_user.js
+++ pages/search_user/search_user.js
@@ -1,8 +1,7 @@
-"use strict";
 let arrow = document.querySelectorAll(".arrow");
 for (var i = 0; i < arrow.length; i++) {
     arrow[i].addEventListener("click", (e) => {
-        let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+        let arrowParent = e.target.parentElement.parentElement;
         arrowParent.classList.toggle("showMenu");
     });
 }
